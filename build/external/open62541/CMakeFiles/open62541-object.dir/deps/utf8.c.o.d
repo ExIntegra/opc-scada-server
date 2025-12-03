887d3ab3af8ea0057c10e9ddeb4bc3c9cbeede3d
ExIntegra <exintegra@raspberrypi5>
2025-12-03 22:15:36 +0100

    Ignore build directory

--- build/external/open62541/CMakeFiles/open62541-object.dir/deps/utf8.c.o.d
+++
@@ -1,5 +0,0 @@
-external/open62541/CMakeFiles/open62541-object.dir/deps/utf8.c.o: \
- /home/exintegra/lab_scada/opc_server/external/open62541/deps/utf8.c \
- /usr/include/stdc-predef.h \
- /home/exintegra/lab_scada/opc_server/external/open62541/deps/utf8.h \
- /usr/lib/gcc/aarch64-linux-gnu/12/include/stddef.h
